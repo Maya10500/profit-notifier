b9628adfb70190f94a5e32334a850283afef5a64
Maya10500 <mdeteix@gmail.com>
2020-09-04 20:47:17 +0200

    appel api authentifié + chargement de l'api ky depuis fichier

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import requests
+import json
+
+URLBASE="https://api.guildwars2.com/v2/"
+
 
 def main():
-    print("Coucou")
-    resp = requests.get("https://google.com")
-    print(resp.text)
+#
+    conf=""
+    with open("conf.json","r") as conf_file:
+        conf=json.load(conf_file)
+    resp = requests.get(URLBASE+"commerce/transactions/history/buys?access_token="+conf["apikey"])
+    items = resp.json()
+    for item in items:
+        resp_obj = requests.get(URLBASE + "items/" + str(item["item_id"])+"?lang=fr")
+        print(resp_obj.json()["name"] + " a été acheté au prix EXHORBITANT de "+str(item["price"])+ "et y en avait "+ str(item["quantity"])+".")
+
+
+
 
 
 
