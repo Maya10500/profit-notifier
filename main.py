50f85bb69f1d88751642197183e12c89cdca950d
Maya10500 <mdeteix@gmail.com>
2020-09-06 19:48:38 +0200

    appel api pour le prix actuel + comparaison

--- main.py
+++ main.py
@@ -21,10 +21,22 @@ def main():
                 dict_buys[item["item_id"]][item["price"]] = item["quantity"]
         else :
             dict_buys[item["item_id"]] = {item["price"]:item["quantity"]}
+    ids=""
+    dict_price = {}
+    resp_price_actu = requests.get(URLBASE + "commerce/prices?ids=" + ids[:-1]).json()
+
     for key, value in dict_buys.items():
         resp_obj = requests.get(URLBASE + "items/" + str(key) + "?lang=fr")
+        resp_price_actu = requests.get(URLBASE + "commerce/prices/" + str(key)).json()
+        ids=ids+str(key)+","
         for key2,value2 in value.items():
             print(resp_obj.json()["name"] + " a été acheté au prix EXHORBITANT de "+str(key2)+ " et y en avait "+ str(value2)+".")
+            print("L'objet est à présent au prix de "+ str(resp_price_actu["buys"]["unit_price"]))
+            if resp_price_actu["buys"]["unit_price"]*0.85-key2>0:
+                print("Tu peux vendre "+resp_obj.json()["name"])
+            else:
+                print("Ne vends pas " + resp_obj.json()["name"])
+
 
 
 
